2deabaef244108276bd7507751c9a6792c2c4185
Camel King <camelking@tataimages.com>
2017-06-23 22:27:24 +0800

    mod: use arrow function

---
+++ src/constant.ts
@@ -0,0 +1,2 @@
+
+export type FnPredicate = (x: any) => boolean;
--- src/isArray.ts
+++ src/isArray.ts
@@ -1,7 +1,18 @@
+/**
+ * Checks if value is classified as an Array object.
+ *
+ * @category Language
+ *
+ * First version: June 23, 2017
+ * Last updated : June 23, 2017
+ *
+ * @export
+ * @param {*} input
+ * @returns {boolean}
+ */
+
+import { FnPredicate } from './constant';
 import { theTypeOf } from './theTypeOf';
 
-export function isArray(input: any): boolean {
-
-  return theTypeOf(input) === 'array';
-
-}
+export const isArray: FnPredicate
+  = (input: any) => (theTypeOf(input) === 'array');
