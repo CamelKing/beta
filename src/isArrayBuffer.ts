506ec9efbcee7399c7d1a9b405b3f836caacb55f
Camel King <camelking@tataimages.com>
2017-06-23 22:27:38 +0800

    add: isArrayBuffer()

---
+++ src/isArrayBuffer.ts
@@ -0,0 +1,18 @@
+/**
+ * Checks if value is classified as an ArrayBuffer object.
+ *
+ * @category Language
+ *
+ * First version: June 23, 2017
+ * Last updated : June 23, 2017
+ *
+ * @export
+ * @param {*} input
+ * @returns {boolean}
+ */
+
+import { FnPredicate } from './constant';
+import { theTypeOf } from './theTypeOf';
+
+export const isArrayBuffer: FnPredicate
+  = (input: any) => (theTypeOf(input) === 'arraybuffer');
