d50f942644cc22f64f9cca0862479f3912849d5a
Camel King <camelking@tataimages.com>
2017-06-25 13:19:12 +0800

    ref: clean up typeOf.isXXX()

--- src/typeOf.ts
+++ src/typeOf.ts
@@ -35,36 +35,100 @@ export function typeOf(input: any, check?: string): string | boolean {
 
 // tslint:disable-next-line:no-namespace
 export namespace typeOf {
-  export const isFloat64Array: FnPredicate = (i: any) => (typeOf(i) === 'float64array');
-  export const isFloat32Array: FnPredicate = (i: any) => (typeOf(i) === 'float32array');
-  export const isUint32Array: FnPredicate = (i: any) => (typeOf(i) === 'uint32array');
-  export const isInt32Array: FnPredicate = (i: any) => (typeOf(i) === 'int32array');
-  export const isUint16Array: FnPredicate = (i: any) => (typeOf(i) === 'uint16array');
-  export const isInt16Array: FnPredicate = (i: any) => (typeOf(i) === 'int16array');
-  export const isUint8ClampedArray: FnPredicate = (i: any) => (typeOf(i) === 'uint8clampedarray');
-  export const isUint8Array: FnPredicate = (i: any) => (typeOf(i) === 'uint8array');
-  export const isInt8Array: FnPredicate = (i: any) => (typeOf(i) === 'int8array');
-  export const isRegExp: FnPredicate = (i: any) => typeOf(i, 'regexp');
-  export const isBuffer: FnPredicate = (i: any) => typeOf(i, 'buffer');
-  export const isArrayBuffer: FnPredicate = (i: any) => typeOf(i, 'arraybuffer');
-  export const isMap: FnPredicate = (i: any) => typeOf(i, 'map');
-  export const isWeakMap: FnPredicate = (i: any) => typeOf(i, 'weakmap');
-  export const isPromise: FnPredicate = (i: any) => typeOf(i, 'promise');
-  export const isFunction: FnPredicate = (i: any) => typeOf(i, 'function');
-  export const isError: FnPredicate = (i: any) => typeOf(i, 'error');
-  export const isSet: FnPredicate = (i: any) => typeOf(i, 'set');
-  export const isWeakSet: FnPredicate = (i: any) => typeOf(i, 'weakset');
-  export const isDate: FnPredicate = (i: any) => typeOf(i, 'date');
-  export const isObject: FnPredicate = (i: any) => typeOf(i, 'object');
-  export const isArray: FnPredicate = (i: any) => typeOf(i, 'array');
-  export const isBoolean: FnPredicate = (i: any) => typeOf(i, 'boolean');
-  export const isNumber: FnPredicate = (i: any) => typeOf(i, 'number');
-  export const isString: FnPredicate = (i: any) => typeOf(i, 'string');
-  export const isSymbol: FnPredicate = (i: any) => typeOf(i, 'symbol');
-  export const isNaN: FnPredicate = (i: any) => typeOf(i, 'nan');
-  export const isNull: FnPredicate = (i: any) => typeOf(i, 'null');
-  export const isUndefined: FnPredicate = (i: any) => typeOf(i, 'undefined');
-  export const isFinite: FnPredicate = (i: any) => typeOf(i, 'number') && Number.isFinite(i);
-  export const isInteger: FnPredicate = (i: any) => typeOf.isNumber(i) && i === toInteger(i);
-  export const isNil: FnPredicate = (i: any) => i == null;
+
+  export const isFloat64Array: FnPredicate
+    = (i: any) => typeOf(i, 'float64array');
+
+  export const isFloat32Array: FnPredicate
+    = (i: any) => typeOf(i, 'float32array');
+
+  export const isUint32Array: FnPredicate
+    = (i: any) => typeOf(i, 'uint32array');
+
+  export const isInt32Array: FnPredicate
+    = (i: any) => typeOf(i, 'int32array');
+
+  export const isUint16Array: FnPredicate
+    = (i: any) => typeOf(i, 'uint16array');
+
+  export const isInt16Array: FnPredicate
+    = (i: any) => typeOf(i, 'int16array');
+
+  export const isUint8ClampedArray: FnPredicate
+    = (i: any) => typeOf(i, 'uint8clampedarray');
+
+  export const isUint8Array: FnPredicate
+    = (i: any) => typeOf(i, 'uint8array');
+
+  export const isInt8Array: FnPredicate
+    = (i: any) => typeOf(i, 'int8array');
+
+  export const isRegExp: FnPredicate
+    = (i: any) => typeOf(i, 'regexp');
+
+  export const isBuffer: FnPredicate
+    = (i: any) => typeOf(i, 'buffer');
+
+  export const isArrayBuffer: FnPredicate
+    = (i: any) => typeOf(i, 'arraybuffer');
+
+  export const isMap: FnPredicate
+    = (i: any) => typeOf(i, 'map');
+
+  export const isWeakMap: FnPredicate
+    = (i: any) => typeOf(i, 'weakmap');
+
+  export const isPromise: FnPredicate
+    = (i: any) => typeOf(i, 'promise');
+
+  export const isFunction: FnPredicate
+    = (i: any) => typeOf(i, 'function');
+
+  export const isError: FnPredicate
+    = (i: any) => typeOf(i, 'error');
+
+  export const isSet: FnPredicate
+    = (i: any) => typeOf(i, 'set');
+
+  export const isWeakSet: FnPredicate
+    = (i: any) => typeOf(i, 'weakset');
+
+  export const isDate: FnPredicate
+    = (i: any) => typeOf(i, 'date');
+
+  export const isObject: FnPredicate
+    = (i: any) => typeOf(i, 'object');
+
+  export const isArray: FnPredicate
+    = (i: any) => typeOf(i, 'array');
+
+  export const isBoolean: FnPredicate
+    = (i: any) => typeOf(i, 'boolean');
+
+  export const isNumber: FnPredicate
+    = (i: any) => typeOf(i, 'number');
+
+  export const isString: FnPredicate
+    = (i: any) => typeOf(i, 'string');
+
+  export const isSymbol: FnPredicate
+    = (i: any) => typeOf(i, 'symbol');
+
+  export const isNaN: FnPredicate
+    = (i: any) => typeOf(i, 'nan');
+
+  export const isNull: FnPredicate
+    = (i: any) => typeOf(i, 'null');
+
+  export const isUndefined: FnPredicate
+    = (i: any) => typeOf(i, 'undefined');
+
+  export const isFinite: FnPredicate
+    = (i: any) => typeOf(i, 'number') && Number.isFinite(i);
+
+  export const isInteger: FnPredicate
+    = (i: any) => typeOf.isNumber(i) && i === toInteger(i);
+
+  export const isNil: FnPredicate
+    = (i: any) => i == null;
 }
