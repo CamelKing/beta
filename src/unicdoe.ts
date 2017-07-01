cbcd398a15da843c6e8286937f3ca684826651d7
Camel King <camelking@tataimages.com>
2017-07-02 00:11:14 +0800

    add: unicodeToArray()

--- src/unicdoe.ts
+++ src/unicdoe.ts
@@ -23,7 +23,25 @@ export const rsComboRange: string = rsComboMarksRange + reComboHalfMarksRange + 
 export const rsVarRange: string = '\\ufe0e\\ufe0f';
 
 /** Used to compose unicode capture groups. */
+export const rsAstral: string = `[${rsAstralRange}]`;
+export const rsCombo: string = `[${rsComboRange}]`;
+export const rsFitz: string = '\\ud83c[\\udffb-\\udfff]';
+export const rsModifier: string = `(?:${rsCombo}|${rsFitz})`;
+export const rsNonAstral: string = `[^${rsAstralRange}]`;
+export const rsRegional: string = '(?:\\ud83c[\\udde6-\\uddff]){2}';
+export const rsSurrPair: string = '[\\ud800-\\udbff][\\udc00-\\udfff]';
 export const rsZWJ: string = '\\u200d';
+
+/** Used to compose unicode regexes. */
+export const reOptMod: string = `${rsModifier}?`;
+export const rsOptVar: string = `[${rsVarRange}]?`;
+export const rsOptJoin: string = `(?:${rsZWJ}(?:${[rsNonAstral, rsRegional, rsSurrPair].join('|')})${rsOptVar + reOptMod})*`;
+export const rsSeq: string = rsOptVar + reOptMod + rsOptJoin;
+export const rsNonAstralCombo: string = `${rsNonAstral}${rsCombo}?`;
+export const rsSymbol: string = `(?:${[rsNonAstralCombo, rsCombo, rsRegional, rsSurrPair, rsAstral].join('|')})`;
 
 /** Used to detect strings with [zero-width joiners or code points from the astral planes](http://eev.ee/blog/2015/09/12/dark-corners-of-unicode/). */
 export const reHasUnicode: RegExp = RegExp(`[${rsZWJ + rsAstralRange + rsComboRange + rsVarRange}]`);
+
+/** Used to match [string symbols](https://mathiasbynens.be/notes/javascript-unicode). */
+export const reUnicode: RegExp = RegExp(`${rsFitz}(?=${rsFitz})|${rsSymbol + rsSeq}`, 'g');
