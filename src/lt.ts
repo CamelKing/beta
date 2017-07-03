3644bc884f89387b67d4f235d18d103c75c97c89
Camel King <camelking@tataimages.com>
2017-07-03 13:49:07 +0800

    add: lt()

---
+++ src/lt.ts
@@ -0,0 +1,21 @@
+/**
+ * Checks if value is less than other.
+ *
+ * Note: this behaves differently from lodash version,
+ *       no numerical strings pairs will be compared on
+ *       alphabetical order, rather than converted to 0.
+ *
+ * @category Language
+ *
+ * First version: July 03, 2017
+ * Last updated : July 03, 2017
+ *
+ * @export
+ * @param {(number | string)} input1
+ * @param {(number | string)} input2
+ * @returns {boolean}
+ */
+
+export function lt(input1: number | string, input2: number | string): boolean {
+  return input1 < input2;
+}
