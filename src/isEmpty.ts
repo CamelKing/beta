1dbfd50f711cbb529a57623a46a0de7e30fc70ae
Camel King <camelking@tataimages.com>
2017-07-03 23:08:37 +0800

    ref: isEmpty()

--- src/isEmpty.ts
+++ src/isEmpty.ts
@@ -28,23 +28,26 @@ export function isEmpty(input: any): boolean {
 
   const type: string = typeOf(input);
 
-  // for array like data, check if the length is zero
   if (isArrayLike(input)
     && (type.search(/(float(32|64)|(int|uint)(8|16|32)|uint8clamped|string|array|buffer|arguments)/) !== -1
       || typeOf(input.slice) === 'function')) {
 
+    // for array like data, check if the length is zero
     return !input.length;
 
-  }
+  } else if (type.search(/(map|set)/) !== -1) {
 
-  // for map and set, check if size is zero
-  if (type.search(/(map|set)/) !== -1) return !input.size;
+    // for map and set, check if size is zero
+    return !input.size;
 
-  // for object, check the property chain containing more than just constructor
-  for (const key in input) {
-    if (input.hasOwnProperty(key) && key !== 'constructor') {
-      return false;
+  } else {
+    for (const key in input) {
+      if (input.hasOwnProperty(key) && key !== 'constructor') {
+        // for object, check the property chain (constructor does not count)
+        return false;
+      }
     }
+
   }
 
   // anything else will result in true (empty)
