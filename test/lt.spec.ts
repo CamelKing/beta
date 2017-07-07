7f1f756b07e59d9e242ee668abd02d902f5f18b7
Camel King <camelking@tataimages.com>
2017-07-07 23:58:09 +0800

    ref: lt.spec.ts

--- test/lt.spec.ts
+++ test/lt.spec.ts
@@ -5,52 +5,151 @@ should();
 
 describe(`lt() - @category Language`, () => {
 
-  describe(`should perform Less Than comparison on numeric values`, () => {
+  describe(`should return true if a < b (number)`, () => {
 
     it(`(3,1) => false`, () => {
-
-      const orig1: number = 3;
-      const orig2: number = 1;
-      const input1: number = orig1;
-      const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
+      lt(3, 1).should.equal(false);
     });
 
     it(`(3.01,3) => false`, () => {
-
-      const orig1: number = 3.01;
-      const orig2: number = 3;
-      const input1: number = orig1;
-      const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
+      lt(3.01, 3).should.equal(false);
     });
 
     it(`(3,3) => false`, () => {
-
-      const orig1: number = 3;
-      const orig2: number = 3;
-      const input1: number = orig1;
-      const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
+      lt(3, 3).should.equal(false);
     });
+
+    it(`(1,3) => true`, () => {
+      lt(1, 3).should.equal(true);
+    });
+
+  });
+
+  describe(`should return true if a < b (numerical strings)`, () => {
+
+    it(`('3','1') => false`, () => {
+      lt('3', '1').should.equal(false);
+    });
+
+    it(`('3.01','3') => false`, () => {
+      lt('3.01', '3').should.equal(false);
+    });
+
+    it(`('3','3') => false`, () => {
+      lt('3', '3').should.equal(false);
+    });
+
+    it(`('1','3') => true`, () => {
+      lt('1', '3').should.equal(true);
+    });
+
+  });
+
+  describe(`should return true if a < b (string - alphabetical order)`, () => {
+
+    it(`('z','a') => false`, () => {
+      lt('z', 'a').should.equal(false);
+    });
+
+    it(`('a','z') => true`, () => {
+      lt('a', 'z').should.equal(true);
+    });
+
+    it(`('a','A') => false`, () => {
+      lt('a', 'A').should.equal(false);
+    });
+
+    it(`('a','a') => false`, () => {
+      lt('a', 'a').should.equal(false);
+    });
+
+    it(`('a',' ') => false`, () => {
+      lt('a', ' ').should.equal(false);
+    });
+
+    it(`('a','') => false`, () => {
+      lt('a', '').should.equal(false);
+    });
+
+  });
+
+  describe(`should return true if a < b (mix number/strings)`, () => {
+
+    it(`(3,'1') => false`, () => {
+      lt(3, '1').should.equal(false);
+    });
+
+    it(`('3.01',3) => false`, () => {
+      lt('3.01', 3).should.equal(false);
+    });
+
+    it(`(3,'3') => false`, () => {
+      lt(3, '3').should.equal(false);
+    });
+
+    it(`('1',3) => true`, () => {
+      lt('1', 3).should.equal(true);
+    });
+
+  });
+
+  describe(`should treat null as 0`, () => {
+
+    it(`(null,3) => true`, () => {
+      lt(null, 3).should.equal(true);
+    });
+
+    it(`(3,null) => false`, () => {
+      lt(3, null).should.equal(false);
+    });
+
+    it(`(null,null) => false`, () => {
+      lt(null, null).should.equal(false);
+    });
+
+  });
+
+  describe(`should return false when one/both is undefined/NaN`, () => {
+
+    it(`(undefined,3) => false`, () => {
+      lt(undefined, 3).should.equal(false);
+    });
+
+    it(`(3,undefined) => false`, () => {
+      lt(3, undefined).should.equal(false);
+    });
+
+    it(`(undefined, undefined) => false`, () => {
+      lt(undefined, undefined).should.equal(false);
+    });
+
+    it(`(NaN,3) => false`, () => {
+      lt(NaN, 3).should.equal(false);
+    });
+
+    it(`(3,NaN) => false`, () => {
+      lt(3, NaN).should.equal(false);
+    });
+
+    it(`(NaN, NaN) => false`, () => {
+      lt(NaN, NaN).should.equal(false);
+    });
+
+  });
+
+  describe(`should return false for different types`, () => {
+
+    it(`(3,'hello') => false`, () => {
+      lt(3, 'hello').should.equal(false);
+    });
+
+    it(`('hello',3) => false`, () => {
+      lt('hello', 3).should.equal(false);
+    });
+
+  });
+
+  describe(`should be functional and not mutating any input`, () => {
 
     it(`(1,3) => true`, () => {
 
@@ -58,323 +157,9 @@ describe(`lt() - @category Language`, () => {
       const orig2: number = 3;
       const input1: number = orig1;
       const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
+      lt(input1, input2).should.equal(true);
       input1.should.be.deep.equal(orig1);
       input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(true);
-
-    });
-
-  });
-
-  describe(`should perform Less Than comparison on numerical strings`, () => {
-
-    it(`('3','1') => false`, () => {
-
-      const orig1: string = '3';
-      const orig2: string = '1';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`('3.01','3') => false`, () => {
-
-      const orig1: string = '3.01';
-      const orig2: string = '3';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`('3','3') => false`, () => {
-
-      const orig1: string = '3';
-      const orig2: string = '3';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-
-    it(`('1','3') => true`, () => {
-
-      const orig1: string = '1';
-      const orig2: string = '3';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(true);
-
-    });
-
-  });
-
-  describe(`should perform Less Than comparison on strings on alphabetical order`, () => {
-
-    it(`('z','a') => false`, () => {
-
-      const orig1: string = 'z';
-      const orig2: string = 'a';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`('a','z') => true`, () => {
-
-      const orig1: string = 'a';
-      const orig2: string = 'z';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(true);
-
-    });
-
-    it(`('a','A') => false`, () => {
-
-      const orig1: string = 'a';
-      const orig2: string = 'A';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`('a',' ') => false`, () => {
-
-      const orig1: string = 'a';
-      const orig2: string = ' ';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`('a','') => false`, () => {
-
-      const orig1: string = 'a';
-      const orig2: string = '';
-      const input1: string = orig1.slice(0);
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-  });
-
-  describe(`should perform Less Than comparison on mix number/strings`, () => {
-
-    it(`(3,'1') => false`, () => {
-
-      const orig1: number = 3;
-      const orig2: string = '1';
-      const input1: number = orig1;
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`('3.01',3) => false`, () => {
-
-      const orig1: string = '3.01';
-      const orig2: number = 3;
-      const input1: string = orig1.slice(0);
-      const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`(3,'3') => false`, () => {
-
-      const orig1: number = 3;
-      const orig2: string = '3';
-      const input1: number = orig1;
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-
-    it(`('1',3) => true`, () => {
-
-      const orig1: string = '1';
-      const orig2: number = 3;
-      const input1: string = orig1.slice(0);
-      const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(true);
-
-    });
-
-  });
-
-  describe(`should always treat null input as 0`, () => {
-
-    it(`(null,3) => true`, () => {
-
-      const orig1: string = null;
-      const orig2: number = 3;
-      const input1: string = orig1;
-      const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
-      should().equal(input1, orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(true);
-
-    });
-
-    it(`(3,null) => false`, () => {
-
-      const orig1: number = 3;
-      const orig2: string = null;
-      const input1: number = orig1;
-      const input2: string = orig2;
-      const output: boolean = lt(input1, input2);
-      should().equal(input2, orig2);
-      input1.should.be.deep.equal(orig1);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-  });
-
-  describe(`should always treat undefined input as not comparable`, () => {
-
-    it(`(undefined,3) => false`, () => {
-
-      const orig1: string = undefined;
-      const orig2: number = 3;
-      const input1: string = orig1;
-      const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
-      should().equal(input1, orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`(3,undefined) => false`, () => {
-
-      const orig1: number = 3;
-      const orig2: string = undefined;
-      const input1: number = orig1;
-      const input2: string = orig2;
-      const output: boolean = lt(input1, input2);
-      should().equal(input2, orig2);
-      input1.should.be.deep.equal(orig1);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-  });
-
-  describe(`should always return false on invalid input`, () => {
-
-    it(`(3,'hello') => false`, () => {
-
-      const orig1: number = 3;
-      const orig2: string = 'hello';
-      const input1: number = orig1;
-      const input2: string = orig2.slice(0);
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
-
-    });
-
-    it(`('hello',3) => false`, () => {
-
-      const orig1: string = 'hello';
-      const orig2: number = 3;
-      const input1: string = orig1.slice(0);
-      const input2: number = orig2;
-      const output: boolean = lt(input1, input2);
-      input1.should.be.deep.equal(orig1);
-      input2.should.be.deep.equal(orig2);
-      output.should.not.be.equal(input1);
-      output.should.not.be.equal(input2);
-      output.should.deep.equal(false);
 
     });
 
