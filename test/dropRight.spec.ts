0980c22b7d911decec88962facdabe4f533dbcf4
Camel King <camelking@tataimages.com>
2017-07-07 17:59:57 +0800

    ref: dropRight()

--- test/dropRight.spec.ts
+++ test/dropRight.spec.ts
@@ -8,27 +8,11 @@ describe(`dropRight() - @category Array`, () => {
   describe(`should handle nested array appropriately`, () => {
 
     it(`([[1],[2],[3]],1)=>[[1],[2]]`, () => {
-
-      const orig: number[][] = [[1], [2], [3]];
-      const input: number[][] = orig.slice(0);
-      const output: number[][] = dropRight(input, 1);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(2);
-      output.should.deep.equal([[1], [2]]);
-
+      dropRight([[1], [2], [3]], 1).should.deep.equal([[1], [2]]);
     });
 
-    it(`([[1,2],[3]],1)=>[[1,2]]`, () => {
-
-      const orig: number[][] = [[1, 2], [3]];
-      const input: number[][] = orig.slice(0);
-      const output: number[][] = dropRight(input, 1);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(1);
-      output.should.deep.equal([[1, 2]]);
-
+    it(`([[1],[2,3]],1)=>[[1]]`, () => {
+      dropRight([[1], [2, 3]], 1).should.deep.equal([[1]]);
     });
 
   });
@@ -36,15 +20,7 @@ describe(`dropRight() - @category Array`, () => {
   describe(`should return [] if drop more than entire array`, () => {
 
     it(`([1,2,3],4)=>[]`, () => {
-
-      const orig: number[] = [1, 2, 3];
-      const input: number[] = orig.slice(0);
-      const output: number[] = dropRight(input, 4);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(0);
-      output.should.deep.equal([]);
-
+      dropRight([1, 2, 3], 4).should.deep.equal([]);
     });
 
   });
@@ -52,39 +28,15 @@ describe(`dropRight() - @category Array`, () => {
   describe(`should drop x items on the right`, () => {
 
     it(`([1,2,3],1)=>[1,2]`, () => {
-
-      const orig: number[] = [1, 2, 3];
-      const input: number[] = orig.slice(0);
-      const output: number[] = dropRight(input, 1);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(2);
-      output.should.deep.equal([1, 2]);
-
+      dropRight([1, 2, 3], 1).should.deep.equal([1, 2]);
     });
 
     it(`([1,2,3],2)=>[1]`, () => {
-
-      const orig: number[] = [1, 2, 3];
-      const input: number[] = orig.slice(0);
-      const output: number[] = dropRight(input, 2);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(1);
-      output.should.deep.equal([1]);
-
+      dropRight([1, 2, 3], 2).should.deep.equal([1]);
     });
 
     it(`([1,2,3],3)=>[]`, () => {
-
-      const orig: number[] = [1, 2, 3];
-      const input: number[] = orig.slice(0);
-      const output: number[] = dropRight(input, 3);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(0);
-      output.should.deep.equal([]);
-
+      dropRight([1, 2, 3], 3).should.deep.equal([]);
     });
 
   });
@@ -92,66 +44,43 @@ describe(`dropRight() - @category Array`, () => {
   describe(`should drop last item by default`, () => {
 
     it(`[1,2,3]=>[1,2]`, () => {
-
-      const orig: number[] = [1, 2, 3];
-      const input: number[] = orig.slice(0);
-      const output: number[] = dropRight(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(2);
-      output.should.deep.equal([1, 2]);
-
+      dropRight([1, 2, 3]).should.deep.equal([1, 2]);
     });
 
   });
 
   describe(`should return [] for empty array`, () => {
 
-    it(`null=>[]`, () => {
-
-      const orig: number[] = null;
-      const input: number[] = orig;
-      const output: number[] = dropRight(input);
-      should().not.exist(input);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(0);
-      output.should.deep.equal([]);
-
-    });
-
-    it(`undefined=>[]`, () => {
-
-      const orig: number[] = undefined;
-      const input: number[] = orig;
-      const output: number[] = dropRight(input);
-      should().not.exist(input);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(0);
-      output.should.deep.equal([]);
-
-    });
-
     it(`([],2)=>[]`, () => {
-
-      const orig: number[] = [];
-      const input: number[] = orig.slice(0);
-      const output: number[] = dropRight(input, 1);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(0);
-      output.should.deep.equal([]);
-
+      dropRight([], 2).should.deep.equal([]);
     });
 
     it(`[]=>[]`, () => {
+      dropRight([]).should.deep.equal([]);
+    });
 
-      const orig: number[] = [];
+  });
+
+  describe(`should return [] for null/undefined`, () => {
+
+    it(`undefined=>[]`, () => {
+      dropRight(undefined).should.deep.equal([]);
+    });
+
+    it(`null=>[]`, () => {
+      dropRight(null).should.deep.equal([]);
+    });
+
+  });
+
+  describe(`should be functional and not mutating any input`, () => {
+
+    it(`([1,2,3],1)=>[1,2]`, () => {
+
+      const orig: number[] = [1, 2, 3];
       const input: number[] = orig.slice(0);
-      const output: number[] = dropRight(input);
+      dropRight(input, 1).should.deep.equal([1, 2]);
       input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(0);
-      output.should.deep.equal([]);
 
     });
 
