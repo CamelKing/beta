ed0c45ecd8b9c2504a8cf9829ecade93ab38a508
Camel King <camelking@tataimages.com>
2017-07-07 18:19:26 +0800

    ref: eq.spec.ts

--- test/eq.spec.ts
+++ test/eq.spec.ts
@@ -8,249 +8,141 @@ describe(`eq() - @category Language`, () => {
   describe(`should do a SameValueZero false comparisons`, () => {
 
     it(`0 !== false`, () => {
-      const orig1: number = 0;
-      const orig2: boolean = false;
-      const input1: number = orig1;
-      const input2: boolean = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq(0, false).should.equal(false);
     });
 
     it(`"" !== false`, () => {
-      const orig1: string = '';
-      const orig2: boolean = false;
-      const input1: string = orig1.slice(0);
-      const input2: boolean = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq('', false).should.equal(false);
     });
 
     it(`"" !== 0`, () => {
-      const orig1: string = '';
-      const orig2: number = 0;
-      const input1: string = orig1.slice(0);
-      const input2: number = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq('', 0).should.equal(false);
     });
 
     it(`"0" !== 0`, () => {
-      const orig1: string = '0';
-      const orig2: number = 0;
-      const input1: string = orig1.slice(0);
-      const input2: number = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq('0', 0).should.equal(false);
     });
 
     it(`"17" !== 17`, () => {
-      const orig1: string = '17';
-      const orig2: number = 17;
-      const input1: string = orig1.slice(0);
-      const input2: number = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq('17', 17).should.equal(false);
     });
 
     it(`[1,2] !== '1,2'`, () => {
-      const orig1: number[] = [1, 2];
-      const orig2: string = '1,2';
-      const input1: number[] = orig1;
-      const input2: string = orig2.slice(0);
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq([1, 2], '1,2').should.equal(false);
     });
 
     it(`null !== undefined`, () => {
-      const orig1: string = null;
-      const orig2: string = undefined;
-      const input1: string = orig1;
-      const input2: string = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq(null, undefined).should.equal(false);
     });
 
     it(`null !== false`, () => {
-      const orig1: boolean = null;
-      const orig2: boolean = false;
-      const input1: boolean = orig1;
-      const input2: boolean = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq(null, false).should.equal(false);
     });
 
     it(`undefined !== false`, () => {
-      const orig1: boolean = undefined;
-      const orig2: boolean = false;
-      const input1: boolean = orig1;
-      const input2: boolean = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq(undefined, false).should.equal(false);
     });
 
     it(`{foo:'bar'} !== {foo:'bar'}`, () => {
-      const orig1: object = { foo: 'bar' };
-      const orig2: object = { foo: 'bar' };
-      const input1: object = orig1;
-      const input2: object = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq({ foo: 'bar' }, { foo: 'bar' }).should.equal(false);
+    });
+
+    it(`[1,2,3] !== [1,2,3]`, () => {
+      eq([1, 2, 3], [1, 2, 3]).should.equal(false);
     });
 
     it(`0 !== null`, () => {
-      const orig1: number = 0;
-      const orig2: number = null;
-      const input1: number = orig1;
-      const input2: number = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq(0, null).should.equal(false);
     });
 
     it(`0 !== undefined`, () => {
-      const orig1: number = 0;
-      const orig2: number = undefined;
-      const input1: number = orig1;
-      const input2: number = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(false);
+      eq(0, undefined).should.equal(false);
     });
 
     it(`0 !== NaN`, () => {
-      const orig1: number = 0;
-      const orig2: number = NaN;
-      const input1: number = orig1;
-      const input2: number = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().not.equal(input2, orig2);
-      should().not.equal(orig2, orig2);
-      should().not.equal(input2, input2);
-      output.should.equal(false);
+      eq(0, NaN).should.equal(false);
     });
 
     it(`'foo' !== NaN`, () => {
-      const orig1: string = 'foo';
-      const orig2: number = NaN;
-      const input1: string = orig1.slice(0);
-      const input2: number = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().not.equal(input2, orig2);
-      should().not.equal(orig2, orig2);
-      should().not.equal(input2, input2);
-      output.should.equal(false);
+      eq('foo', NaN).should.equal(false);
     });
 
   });
 
   describe(`should do a SameValueZero true comparisons`, () => {
 
-    it(`undefined === undefined`, () => {
-      const orig: boolean = undefined;
-      const input1: boolean = orig;
-      const input2: boolean = orig;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig);
-      should().equal(input2, orig);
-      output.should.equal(true);
-    });
-
-    it(`null === null`, () => {
-      const orig: boolean = null;
-      const input1: boolean = orig;
-      const input2: boolean = orig;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig);
-      should().equal(input2, orig);
-      output.should.equal(true);
-    });
-
     it(`true === true`, () => {
-      const orig: boolean = true;
-      const input1: boolean = orig;
-      const input2: boolean = orig;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig);
-      should().equal(input2, orig);
-      output.should.equal(true);
+      eq(true, true).should.equal(true);
     });
 
     it(`false === false`, () => {
-      const orig: boolean = false;
-      const input1: boolean = orig;
-      const input2: boolean = orig;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig);
-      should().equal(input2, orig);
-      output.should.equal(true);
+      eq(false, false).should.equal(true);
     });
 
     it(`'hello' === 'hello'`, () => {
-      const orig: string = 'hello';
-      const input1: string = orig;
-      const input2: string = orig;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig);
-      should().equal(input2, orig);
-      output.should.equal(true);
+      eq('hello', 'hello').should.equal(true);
     });
 
     it(`0 === 0`, () => {
-      const orig: number = 0;
-      const input1: number = orig;
-      const input2: number = orig;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig);
-      should().equal(input2, orig);
-      output.should.equal(true);
+      eq(0, 0).should.equal(true);
     });
 
     it(`+0 === -0`, () => {
-      const orig1: number = +0;
-      const orig2: number = -0;
-      const input1: number = orig1;
-      const input2: number = orig2;
-      const output: boolean = eq(input1, input2);
-      should().equal(input1, orig1);
-      should().equal(input2, orig2);
-      output.should.equal(true);
+      eq(+0, -0).should.equal(true);
+    });
+
+    it(`a === a (object)`, () => {
+      const a: object = { foo: 'bar' };
+      eq(a, a).should.equal(true);
+    });
+
+    it(`a === a (array)`, () => {
+      const a: number[] = [1, 2, 3];
+      eq(a, a).should.equal(true);
+    });
+
+    it(`a === a (string)`, () => {
+      const a: string = 'hello';
+      eq(a, a).should.equal(true);
+    });
+
+  });
+
+  describe(`should compare null/undefined/NaN correctly`, () => {
+
+    it(`undefined === undefined`, () => {
+      eq(undefined, undefined).should.equal(true);
+    });
+
+    it(`null === null`, () => {
+      eq(null, null).should.equal(true);
     });
 
     it(`NaN === NaN`, () => {
-      const orig: number = NaN;
-      const input1: number = orig;
-      const input2: number = orig;
-      const output: boolean = eq(input1, input2);
-      should().not.equal(orig, orig);
-      should().not.equal(input1, orig);
-      should().not.equal(input2, orig);
-      should().not.equal(input1, input1);
-      should().not.equal(input2, input2);
-      output.should.equal(true);
+      eq(NaN, NaN).should.equal(true);
+    });
+
+  });
+
+  describe(`should be functional and not mutating any input`, () => {
+
+    it(`'hello' === 'hello'`, () => {
+      const orig: string = 'hello';
+      const input1: string = orig.slice(0);
+      const input2: string = orig.slice(0);
+      eq(input1, input2).should.equal(true);
+      input1.should.deep.equal(orig);
+      input2.should.deep.equal(orig);
+    });
+
+    it(`100 !== 200`, () => {
+      const orig1: number = 100;
+      const orig2: number = 200;
+      const input1: number = orig1;
+      const input2: number = orig2;
+      eq(input1, input2).should.equal(false);
+      input1.should.deep.equal(orig1);
+      input2.should.deep.equal(orig2);
     });
 
   });
