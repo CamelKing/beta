b9a36e463e5614e4a8477c872ae48b13ac72eb68
Camel King <camelking@tataimages.com>
2017-07-07 23:16:35 +0800

    ref: isSafeInteger.spec.ts

--- test/isSafeInteger.spec.ts
+++ test/isSafeInteger.spec.ts
@@ -5,26 +5,38 @@ should();
 
 describe(`isSafeInteger() - @category Language`, () => {
 
-  describe(`should check for integer number`, () => {
+  describe(`should return true for integer within safe range`, () => {
 
     it(`3 => true`, () => {
       isSafeInteger(3).should.equal(true);
-    });
-
-    it(`Number.MIN_VALUE => false`, () => {
-      isSafeInteger(Number.MIN_VALUE).should.equal(false);
     });
 
     it(`9007199254740991 => true`, () => {
       isSafeInteger(9007199254740991).should.equal(true);
     });
 
-    it(`9007199254740992 => false`, () => {
-      isSafeInteger(9007199254740992).should.equal(false);
-    });
-
     it(`-9007199254740991 => true`, () => {
       isSafeInteger(-9007199254740991).should.equal(true);
+    });
+
+  });
+
+  describe(`should return false for non integers`, () => {
+
+    it(`Number.MIN_VALUE => false`, () => {
+      isSafeInteger(Number.MIN_VALUE).should.equal(false);
+    });
+
+    it(`123.456 => false`, () => {
+      isSafeInteger(123.456).should.equal(false);
+    });
+
+  });
+
+  describe(`should return false for numbers out of range`, () => {
+
+    it(`9007199254740992 => false`, () => {
+      isSafeInteger(9007199254740992).should.equal(false);
     });
 
     it(`-9007199254740992 => false`, () => {
@@ -39,9 +51,9 @@ describe(`isSafeInteger() - @category Language`, () => {
       isSafeInteger(-Infinity).should.equal(false);
     });
 
-    it(`'3' => false`, () => {
-      isSafeInteger('3').should.equal(false);
-    });
+  });
+
+  describe(`should return false for null/undefined/NaN`, () => {
 
     it(`null => false`, () => {
       isSafeInteger(null).should.equal(false);
@@ -53,6 +65,19 @@ describe(`isSafeInteger() - @category Language`, () => {
 
     it(`undefined => false`, () => {
       isSafeInteger(undefined).should.equal(false);
+    });
+
+  });
+
+  describe(`should be functional and not mutating any input`, () => {
+
+    it(`123 => true`, () => {
+
+      const orig: any = 123;
+      const input: any = orig;
+      isSafeInteger(input).should.equal(true);
+      input.should.be.deep.equal(orig);
+
     });
 
   });
