e79009f76ebd18e2110661f746eeaa7625cbd997
Camel King <camelking@tataimages.com>
2017-07-07 15:12:25 +0800

    ref: castArray.spec.ts

--- test/castArray.spec.ts
+++ test/castArray.spec.ts
@@ -8,19 +8,11 @@ describe(`castArray() - @category Language`, () => {
   describe(`should turn multiple arguments (mixed types) into array`, () => {
 
     it(`(1,2,3)=>[1,2,3]`, () => {
-
-      const output: number[] = castArray(1, 2, 3);
-      output.should.have.lengthOf(3);
-      output.should.deep.equal([1, 2, 3]);
-
+      castArray(1, 2, 3).should.deep.equal([1, 2, 3]);
     });
 
-    it(`(false,'2',3)=>[false,'2',3]`, () => {
-
-      const output: any[] = castArray(false, '2', 3);
-      output.should.have.lengthOf(3);
-      output.should.deep.equal([false, '2', 3]);
-
+    it(`(false,'hello',3)=>[false,'hello',3]`, () => {
+      castArray(false, 'hello', 3).should.deep.equal([false, 'hello', 3]);
     });
 
   });
@@ -28,15 +20,7 @@ describe(`castArray() - @category Language`, () => {
   describe(`should return array being passed in as is`, () => {
 
     it(`[1,2,3]=>[1,2,3]`, () => {
-
-      const orig: number[] = [1, 2, 3];
-      const input: number[] = orig.slice(0);
-      const output: number[] = castArray(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(3);
-      output.should.deep.equal([1, 2, 3]);
-
+      castArray([1, 2, 3]).should.deep.equal([1, 2, 3]);
     });
 
   });
@@ -44,51 +28,19 @@ describe(`castArray() - @category Language`, () => {
   describe(`should cast all non-array items into an array`, () => {
 
     it(`({a:1})=>[{a:1}]`, () => {
-
-      const orig: object = { a: 1 };
-      const input: object = { a: 1 };
-      const output: object[] = castArray(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(1);
-      output.should.deep.equal([{ a: 1 }]);
-
+      castArray({ a: 1 }).should.deep.equal([{ a: 1 }]);
     });
 
     it(`(1)=>[1]`, () => {
-
-      const orig: number = 1;
-      const input: number = orig;
-      const output: number[] = castArray(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(1);
-      output.should.deep.equal([1]);
-
+      castArray(1).should.deep.equal([1]);
     });
 
     it(`('hello')=>['hello']`, () => {
-
-      const orig: string = 'hello';
-      const input: string = orig.slice(0);
-      const output: string[] = castArray(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(1);
-      output.should.deep.equal(['hello']);
-
+      castArray('hello').should.deep.equal(['hello']);
     });
 
     it(`(true)=>[true]`, () => {
-
-      const orig: boolean = true;
-      const input: boolean = orig;
-      const output: boolean[] = castArray(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(1);
-      output.should.deep.equal([true]);
-
+      castArray(true).should.deep.equal([true]);
     });
 
   });
@@ -96,11 +48,7 @@ describe(`castArray() - @category Language`, () => {
   describe(`should return [] if no param passed`, () => {
 
     it(`()=>[]`, () => {
-
-      const output: any[] = castArray();
-      output.should.have.lengthOf(0);
-      output.should.deep.equal([]);
-
+      castArray().should.deep.equal([]);
     });
 
   });
@@ -108,24 +56,44 @@ describe(`castArray() - @category Language`, () => {
   describe(`should cast null and undefined into an array`, () => {
 
     it(`(null)=>[null]`, () => {
-
-      const input: number = null;
-      const output: number[] = castArray(input);
-      should().not.exist(input);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(1);
-      output.should.deep.equal([null]);
-
+      castArray(null).should.deep.equal([null]);
     });
 
     it(`(undefined)=>[undefined]`, () => {
+      castArray(undefined).should.deep.equal([undefined]);
+    });
 
-      const input: number = undefined;
-      const output: number[] = castArray(input);
-      should().not.exist(input);
-      output.should.not.be.equal(input);
-      output.should.have.lengthOf(1);
-      output.should.deep.equal([undefined]);
+  });
+
+  describe(`should be functional and not mutating any input`, () => {
+
+    it(`(1,2,3)=>[1,2,3]`, () => {
+
+      const orig1: number = 1;
+      const orig2: number = 2;
+      const orig3: number = 3;
+      const input1: number = 1;
+      const input2: number = 2;
+      const input3: number = 3;
+      castArray(input1, input2, input3).should.deep.equal([1, 2, 3]);
+      input1.should.deep.equal(orig1);
+      input2.should.deep.equal(orig2);
+      input3.should.deep.equal(orig3);
+
+    });
+
+    it(`(false,'hello',3)=>[false,'hello',3]`, () => {
+
+      const orig1: boolean = false;
+      const orig2: string = 'hello';
+      const orig3: number = 3;
+      const input1: boolean = orig1;
+      const input2: string = orig2;
+      const input3: number = orig3;
+      castArray(input1, input2, input3).should.deep.equal([false, 'hello', 3]);
+      input1.should.deep.equal(orig1);
+      input2.should.deep.equal(orig2);
+      input3.should.deep.equal(orig3);
 
     });
 
