d170150d7b62f5936d412d5c9b75cfd8dbbe9cf7
Camel King <camelking@tataimages.com>
2017-07-08 19:55:06 +0800

    ref: toString.spec.ts

--- test/toString.spec.ts
+++ test/toString.spec.ts
@@ -5,197 +5,146 @@ should();
 
 describe(`toString() - @category Language`, () => {
 
-  describe(`should return value as string`, () => {
+  describe(`should return string representation of primitives`, () => {
+
+    it(`123 => '123'`, () => {
+      toString(123).should.deep.equal('123');
+    });
+
+    it(`'hello' => 'hello'`, () => {
+      toString('hello').should.deep.equal('hello');
+    });
+
+    it(`true => 'true'`, () => {
+      toString(true).should.deep.equal('true');
+    });
+
+    it(`-0 => '-0'`, () => {
+      toString(-0).should.deep.equal('-0');
+    });
+
+  });
+
+  describe(`should return string representation of the function`, () => {
+
+    it(`()=>-123 => '() => -123'`, () => {
+      const f: () => number = () => -123;
+      toString(f).should.deep.equal('() => -123');
+    });
+
+    it(`(x)=>Math.abs(x) => '(x) => Math.abs(x)'`, () => {
+      const f: (x: number) => number = (x: number) => Math.abs(x);
+      toString(f).should.deep.equal('(x) => Math.abs(x)');
+    });
+
+    it(`Math.abs => 'function abs() { [native code] }'`, () => {
+      toString(Math.abs).should.deep.equal(Math.abs.toString());
+    });
+
+  });
+
+  describe(`should return date string for date objects`, () => {
+
+    it(`Date() => '{"a":1}'`, () => {
+      const d: Date = new Date();
+      toString(d).should.deep.equal(d.toString());
+    });
+
+  });
+
+  describe(`should return contents in string for objects`, () => {
+
+    it(`new Object(1) => '1'`, () => {
+      toString(new Object(1)).should.deep.equal('1');
+    });
+
+    it(`{a:1} => '{"a":1}'`, () => {
+      toString({ a: 1 }).should.deep.equal('{"a":1}');
+    });
+
+  });
+
+  describe(`should return name of objects for symbol/promise`, () => {
+
+    it(`Symbol() => 'Symbol()'`, () => {
+      toString(Symbol()).should.deep.equal('Symbol()');
+    });
+
+    it(`Promise.resolve('hello') => 'Promise()'`, () => {
+      const p: Promise<string> = Promise.resolve('hello');
+      toString(p).should.deep.equal('Promise()');
+    });
+
+    it(`Error() => 'Error Message + Error Stack'`, () => {
+      const e: Error = new Error('TEST ERROR');
+      toString(e).should.deep.equal(`${e.message}\n${e.stack}`);
+    });
+
+  });
+
+  describe(`should return contents of types array in string`, () => {
+
+    it(`new Int8Array() => '0,0'`, () => {
+      toString(new Int8Array(2)).should.deep.equal('0,0');
+    });
+
+    it(`new Int16Array() => '0,0,0,0'`, () => {
+      toString(new Int8Array(4)).should.deep.equal('0,0,0,0');
+    });
+
+    it(`new Float32Array() => '0,0'`, () => {
+      toString(new Int8Array(2)).should.deep.equal('0,0');
+    });
+
+  });
+
+  describe(`should return contents in string for (nested) array`, () => {
+
+    it(`[1,2,3] => '1,2,3'`, () => {
+      toString([1, 2, 3]).should.deep.equal('1,2,3');
+    });
+
+    it(`[1,[2,3]] => '1,2,3'`, () => {
+      toString([1, [2, 3]]).should.deep.equal('1,2,3');
+    });
+
+  });
+
+  describe(`should return 'NaN' for NaN`, () => {
+
+    it(`NaN => 'NaN'`, () => {
+      toString(NaN).should.deep.equal('NaN');
+    });
+
+  });
+
+  describe(`should return 'null' for null`, () => {
+
+    it(`null => 'null'`, () => {
+      toString(null).should.deep.equal('null');
+    });
+
+  });
+
+  describe(`should return 'undefined' for undefined`, () => {
+
+    it(`undefined => 'undefined'`, () => {
+      toString(undefined).should.deep.equal('undefined');
+    });
+
+  });
+
+  describe(`should be functional and not mutating any input`, () => {
 
     it(`123 => '123'`, () => {
 
       const orig: number = 123;
       const input: number = orig;
-      const output: string = toString(input);
+      toString(input).should.deep.equal('123');
       input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('123');
-
-    });
-
-    it(`NaN => 'NaN'`, () => {
-
-      const orig: number = NaN;
-      const input: number = orig;
-      const output: string = toString(input);
-      input.should.not.equal(input);
-      input.should.not.equal(orig);
-      orig.should.not.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('NaN');
-
-    });
-
-    it(`'hello' => 'hello'`, () => {
-
-      const orig: string = 'hello';
-      const input: string = orig;
-      const output: string = toString(input);
-      input.should.be.deep.equal(orig);
-      output.should.deep.equal('hello');
-
-    });
-
-    it(`true => 'true'`, () => {
-
-      const orig: boolean = true;
-      const input: boolean = orig;
-      const output: string = toString(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('true');
-
-    });
-
-    it(`-0 => '-0'`, () => {
-
-      const orig: number = -0;
-      const input: number = orig;
-      const output: string = toString(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('-0');
-
-    });
-
-    it(`Symbol() => 'Symbol()'`, () => {
-
-      const orig: symbol = Symbol();
-      const input: symbol = orig;
-      const output: string = toString(input);
-      should().equal(input, orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('Symbol()');
-
-    });
-
-    it(`Promise.resolve('hello') => 'Promise()'`, () => {
-
-      const orig: Promise<string> = Promise.resolve('hello');
-      const input: Promise<string> = orig;
-      const output: string = toString(input);
-      should().equal(input, orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('Promise()');
-
-    });
-
-    it(`()=>-123 => '-123'`, () => {
-
-      const orig: () => number = () => -123;
-      const input: () => number = orig;
-      const output: string = toString(input);
-      should().equal(input, orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('-123');
-
-    });
-
-    it(`{a:1} => '{"a":1}'`, () => {
-
-      const orig: object = { a: 1 };
-      const input: object = orig;
-      const output: string = toString(input);
-      input.should.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('{"a":1}');
-
-    });
-
-    it(`Error() => 'Error()'`, () => {
-
-      const orig: Error = new Error();
-      const input: object = orig;
-      const output: string = toString(input);
-      input.should.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('Error()');
-
-    });
-
-    it(`Date() => '{"a":1}'`, () => {
-
-      const orig: Date = new Date();
-      const input: Date = orig;
-      const output: string = toString(input);
-      input.should.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(orig.toString());
-
-    });
-
-    it(`new Int8Array() => '0,0'`, () => {
-
-      const orig: Int8Array = new Int8Array(2);
-      const input: object = orig;
-      const output: string = toString(input);
-      input.should.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('0,0');
 
     });
 
   });
-
-  describe(`should return (nested) array as a series of string`, () => {
-
-    it(`[1,2,3] => '1,2,3'`, () => {
-
-      const orig: number[] = [1, 2, 3];
-      const input: number[] = orig.slice(0);
-      const output: string = toString(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('1,2,3');
-
-    });
-
-    it(`[1,[2,3]] => '1,2,3'`, () => {
-
-      const orig: any[] = [1, [2, 3]];
-      const input: any[] = orig.slice(0);
-      const output: string = toString(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('1,2,3');
-
-    });
-
-  });
-
-  /*
-
-  describe(`should return '' for invalid value`, () => {
-
-    it(`null => ''`, () => {
-
-      const orig: number = null;
-      const input: number = orig;
-      const output: string = toString(input);
-      should().equal(input, orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('');
-
-    });
-
-    it(`undefined => ''`, () => {
-
-      const orig: number = undefined;
-      const input: number = orig;
-      const output: string = toString(input);
-      should().equal(input, orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal('');
-
-    });
-
-  });
-  
-  */
 
 });
