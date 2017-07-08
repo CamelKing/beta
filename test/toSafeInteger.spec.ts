ee6322370b574ec79c44cb5899efaa71a00f0201
Camel King <camelking@tataimages.com>
2017-07-08 19:54:46 +0800

    ref: toSafeInteger.spec.ts

--- test/toSafeInteger.spec.ts
+++ test/toSafeInteger.spec.ts
@@ -5,7 +5,90 @@ should();
 
 describe(`toSafeInteger() - @category Language`, () => {
 
-  describe(`should convert an input into a safe integer`, () => {
+  describe(`should return number as integer`, () => {
+
+    it(`3 => 3`, () => {
+      toSafeInteger(3).should.equal(3);
+    });
+
+    it(`3.2 => 3`, () => {
+      toSafeInteger(3.2).should.equal(3);
+    });
+
+  });
+
+  describe(`should return valid integer for numerical string`, () => {
+
+    it(`"3" => 3`, () => {
+      toSafeInteger('3').should.equal(3);
+    });
+
+    it(`"3.2" => 3`, () => {
+      toSafeInteger('3.2').should.equal(3);
+    });
+
+  });
+
+  describe(`should return 0 for non numerical string`, () => {
+
+    it(`"Hello" => 0`, () => {
+      toSafeInteger('hello').should.equal(0);
+    });
+
+  });
+
+  describe(`should return between -9007199254740991 and 9007199254740991`, () => {
+
+    it(`Number.MAX_VALUE => 9007199254740991`, () => {
+      toSafeInteger(Number.MAX_VALUE).should.equal(9007199254740991);
+    });
+
+    it(`Number.MIN_VALUE => 0`, () => {
+      toSafeInteger(Number.MIN_VALUE).should.equal(0);
+    });
+
+    it(`Infinity => 9007199254740991`, () => {
+      toSafeInteger(Infinity).should.equal(9007199254740991);
+    });
+
+    it(`+Infinity => +9007199254740991`, () => {
+      toSafeInteger(+Infinity).should.equal(+9007199254740991);
+    });
+
+    it(`-Infinity => -9007199254740991`, () => {
+      toSafeInteger(-Infinity).should.equal(-9007199254740991);
+    });
+
+  });
+
+  describe(`should return 0 for NaN`, () => {
+
+    it(`NaN => 0`, () => {
+      toSafeInteger(NaN).should.equal(0);
+    });
+
+  });
+
+  /*
+
+    July 08 2017
+    Taken care of by StrictNullChecks
+
+    describe(`should return 0 for null/undefined`, () => {
+
+      it(`null => 0`, () => {
+       toSafeInteger(null).should.equal(0);
+      });
+
+      it(`undefined => 0`, () => {
+        toSafeInteger(undefined).should.equal(0);
+      });
+
+    });
+
+  */
+
+  describe(`should be functional and not mutating any input`, () => {
 
     it(`3.2 => 3`, () => {
 
@@ -13,139 +96,7 @@ describe(`toSafeInteger() - @category Language`, () => {
       const input: number = orig;
       const output: number = toSafeInteger(input);
       input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
       output.should.deep.equal(3);
-
-    });
-
-    it(`3 => 3`, () => {
-
-      const orig: number = 3;
-      const input: number = orig;
-      const output: number = toSafeInteger(input);
-      input.should.be.deep.equal(orig);
-      output.should.deep.equal(3);
-
-    });
-
-    it(`"3.2" => 3`, () => {
-
-      const orig: string = '3.2';
-      const input: string = orig.slice(0);
-      const output: number = toSafeInteger(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(3);
-
-    });
-
-    it(`"3" => 3`, () => {
-
-      const orig: string = '3';
-      const input: string = orig.slice(0);
-      const output: number = toSafeInteger(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(3);
-
-    });
-
-  });
-
-  describe(`should return 0 for invalid input`, () => {
-
-    it(`"hello" => 0`, () => {
-
-      const orig: string = 'hello';
-      const input: string = orig.slice(0);
-      const output: number = toSafeInteger(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(0);
-
-    });
-
-    it(`NaN => 0`, () => {
-
-      const orig: number = NaN;
-      const input: number = orig;
-      const output: number = toSafeInteger(input);
-      input.should.not.equal(input);
-      orig.should.not.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(0);
-
-    });
-
-    /*
-        it(`null => 0`, () => {
-
-          const orig: number = null;
-          const input: number = orig;
-          const output: number = toSafeInteger(input);
-          should().equal(input, orig);
-          output.should.not.be.equal(input);
-          output.should.deep.equal(0);
-
-        });
-
-        it(`undefined => 0`, () => {
-
-          const orig: number = undefined;
-          const input: number = orig;
-          const output: number = toSafeInteger(input);
-          should().equal(input, orig);
-          output.should.not.be.equal(input);
-          output.should.deep.equal(0);
-
-        });
-    */
-
-  });
-
-  describe(`should handle extreme boundaries`, () => {
-
-    it(`Number.MIN_VALUE => 0`, () => {
-
-      const orig: number = Number.MIN_VALUE;
-      const input: number = orig;
-      const output: number = toSafeInteger(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(0);
-
-    });
-
-    it(`Number.MAX_VALUE => 9007199254740991`, () => {
-
-      const orig: number = Number.MAX_VALUE;
-      const input: number = orig;
-      const output: number = toSafeInteger(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(Number.MAX_SAFE_INTEGER);
-
-    });
-
-    it(`-Infinity => -9007199254740991`, () => {
-
-      const orig: number = -Infinity;
-      const input: number = orig;
-      const output: number = toSafeInteger(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(Number.MIN_SAFE_INTEGER);
-
-    });
-
-    it(`Infinity => 9007199254740991`, () => {
-
-      const orig: number = Infinity;
-      const input: number = orig;
-      const output: number = toSafeInteger(input);
-      input.should.be.deep.equal(orig);
-      output.should.not.be.equal(input);
-      output.should.deep.equal(Number.MAX_SAFE_INTEGER);
 
     });
 
